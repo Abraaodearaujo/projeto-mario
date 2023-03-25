590d87c96bc0405ebe5be7e0d3bf7de79a862b5e
Abraão Silva de Araújo <121314336+Abraaodearaujo@users.noreply.github.com>
2023-03-25 18:12:14 -0300

    tentando incrementar um sistema de adição de pontos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
+
+
 const jump = () => {
     mario.classList.add('jump');
 
@@ -14,7 +16,7 @@ const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPisition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    console.log(marioPisition)
+
     if(pipePosition <= 44 && pipePosition > 0  && marioPisition < 65) {
 
         pipe.style.animation = 'none';
@@ -26,6 +28,8 @@ const loop = setInterval(() => {
         mario.src = 'imagens/game-over.png'
         mario.style.marginLeft = '-5px'
 
+    }else {
+        
     }
 }, 10);
 
